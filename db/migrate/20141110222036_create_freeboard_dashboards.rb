1d39df6502532957700b163ffadf98f80683973d
Darren Cauthon <darren@cauthon.com>
2014-11-10 13:48:07 -0600

    Move the engine to the root of this project.

--- db/migrate/20141110222036_create_freeboard_dashboards.rb
+++
@@ -1,2 +0,0 @@
-class CreateFreeboardDashboards < Freeboard::Migration
-end
--- freeboard/config/routes.rb
+++
@@ -1,5 +0,0 @@
-Freeboard::Engine.routes.draw do
-  get ':key'              => 'dashboard#index'
-  post '_save_board/:key' => 'dashboard#save_board'
-  get  '_get_board/:key'  => 'dashboard#get_board'
-end
