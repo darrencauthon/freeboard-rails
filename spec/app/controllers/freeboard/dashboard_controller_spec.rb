062e4bfb311d3c4489ebcf6397b1c70db2019434
Darren Cauthon <darren@cauthon.com>
2014-12-04 17:02:58 -0600

    First test through the dashboard controller.

---
+++ spec/app/controllers/freeboard/dashboard_controller_spec.rb
@@ -0,0 +1,27 @@
+require_relative '../../../minitest_helper'
+
+describe Freeboard::DashboardController do
+
+  let(:controller) do
+    c = Freeboard::DashboardController.new
+    c.stubs(:params).returns params
+    c
+  end
+
+  let(:params) { {} }
+
+  before do
+    Freeboard::Dashboard.delete_all
+  end
+
+  describe "index" do
+    describe "no dashboard exists" do
+      it "should return a blank dashboard" do
+        controller.index
+        dashboard = controller.instance_eval { @dashboard }
+        dashboard.is_a?(Freeboard::Dashboard)
+      end
+    end
+  end
+
+end
